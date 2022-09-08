f6097331dc8a6534dc1a32337d6bcfd3bafcfd33
devaward <devawardani@gmail.com>
2022-09-09 00:46:54 +0700

    ke4

--- zippy.php
+++ zippy.php
@@ -12,7 +12,7 @@ header('Access-Control-Allow-Origin: *');
 
 if(!empty($_GET['zp'])) {
   $url = json_decode(decrypt($_GET['zp'], $password_salt))->url;
-  $json = file_get_contents('https://node' . $item . '/zippy?url=' . $url);
+  $json = file_get_contents('https://node' . $item . '.herokuapp.com/zippy?url=' . $url);
   echo $json;
 } else {
   header("HTTP/1.1 403 Forbidden" );
