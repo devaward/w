737b311cba7cc0c0e1675f9503163a45ecd137b1
devaward <devawardani@gmail.com>
2022-09-09 03:27:13 +0700

    ke5

---
+++ vendor/composer/installed.php
@@ -0,0 +1,146 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0a9441dda72e48f15f73e74480d779d4963785e4',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0a9441dda72e48f15f73e74480d779d4963785e4',
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '7.5.0',
+      'version' => '7.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b50a2a1251152e43f6a37f0fa053e730a67d25ba',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '1.5.2',
+      'version' => '1.5.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b94b2807d85443f9719887892882d0329d1e2598',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '1.9.0',
+      'version' => '1.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e98e3e6d4f86621a9b75f623996e6bbdeb4b9318',
+    ),
+    'myclabs/php-enum' => 
+    array (
+      'pretty_version' => '1.8.4',
+      'version' => '1.8.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a867478eae49c9f59ece437ae7f9506bfaa27483',
+    ),
+    'paquettg/php-html-parser' => 
+    array (
+      'pretty_version' => '3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4e01a438ad5961cc2d7427eb9798d213c8a12629',
+    ),
+    'paquettg/string-encode' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a8708e9fac9d5ddfc8fc2aac6004e2cd05d80fee',
+    ),
+    'php-http/httplug' => 
+    array (
+      'pretty_version' => '2.3.0',
+      'version' => '2.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f640739f80dfa1152533976e3c112477f69274eb',
+    ),
+    'php-http/promise' => 
+    array (
+      'pretty_version' => '1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4c4c1f9b7289a2ec57cde7f1e9762a5789506f88',
+    ),
+    'psr/http-client' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
+    ),
+    'psr/http-client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v2.5.2',
+      'version' => '2.5.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e8b495ea28c1d97b5e0c121748d6f9b53d075c66',
+    ),
+  ),
+);
--- zippy.php
+++ zippy.php
@@ -1,29 +1,70 @@
 <?php
 
-/* [Fungsi] */
-include 'functions.php';
-
-/* [Random Server] */
-$server = [2000,2100,2101,2102,2103];
-$item = $server[array_rand($server)];
-$password_salt = 'Jona Ganteng';
-
 header('Access-Control-Allow-Origin: *');
 
+/* [Fungsi] */
+include 'functions.php';
+require "vendor/autoload.php";
+use PHPHtmlParser\Dom;
+use PHPHtmlParser\Options;
+
 if(!empty($_GET['zp'])) {
+  /*$parse = parse_url($_GET['url']);
+  $host  = $parse['host'];
+  $get_id = explode('/',$parse['path'])[2];
+  $create_fakeDL = 'https://' . $host . '/d/' . $get_id . '/123/jancox';
+  $html =  file_get_contents($create_fakeDL);*/
+
+  /* [Buat Dom] */
   $url = json_decode(decrypt($_GET['zp'], $password_salt))->url;
-  $json = file_get_contents('https://node' . $item . '.herokuapp.com/zippy?url=' . $url);
-  $decode = json_decode($json);
-  $extension = end(explode('.', $decode->title));
-  if($extension === 'mp4') {
-    /*ini_set("max_execution_time", 0);
-    $stream = new VideoStream($decode->url);
-    $stream->start();*/
-    echo file_get_contents($decode->url);
-  } else {
-    echo 'Extension not mp4';
+  $html = file_get_contents($url);
+  $dom = new Dom;
+  $dom->setOptions(
+    (new Options())
+        ->setRemoveScripts(false)
+);
+  $dom->loadStr($html);
+
+  /* [Cari Script] */
+  $scripts = $dom->find('script');
+  $array_script;
+  $find = "Math.pow";
+  foreach ($scripts as $script) {
+    if(strpos($script->text, $find) !== false) {
+      $array_script = $script;
+    }
   }
+
+  /* [Cari title] */
+  $metas = $dom->find('meta');
+  $title;
+  foreach ($metas as $meta) {
+    if($meta->getAttribute('property') !== false) {
+      if($meta->getAttribute('property') === 'og:title') {
+        $title = $meta->getAttribute('content');
+      }
+    }
+  }
+
+  /* [settingan menyesuaikan logika zippyshare, mungkin sewaktu" bisa berubah] */
+  $text = $array_script->text;
+  $regex = '/var a =(.*); document.getElementById\(\'dlbutton\'\).omg(.*)/s';
+  $get_id = preg_replace($regex,'$1', $text);
+  $substring = substr('asdasd',0 , 3);
+  $length = strlen($substring);
+  $id_pow = pow($get_id, 3) + $length;
+
+  /* [setelah menemukan hasil dari pow, buat link DL] */
+  $parse = parse_url($_GET['url']);
+  $host  = $parse['host'];
+  $get_id = explode('/',$parse['path'])[2];
+  $link_DL = 'https://'.$host.'/d/'.$get_id.'/'.$id_pow.'/'.rawurlencode(trim($title));
+
+  /* [Buat Link Streaming] */
+  ini_set("max_execution_time", 0);
+  $stream = new VideoStream($decode->url);
+  $stream->start();
 } else {
-  header("HTTP/1.1 403 Forbidden" );
-  exit;
+    header("HTTP/1.1 403 Forbidden" );
+    exit;
 }
