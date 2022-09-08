812a254bc52eb3544055067506b0e45b7d01e747
devaward <devawardani@gmail.com>
2022-09-09 00:54:26 +0700

    ke4

--- zippy.php
+++ zippy.php
@@ -13,7 +13,15 @@ header('Access-Control-Allow-Origin: *');
 if(!empty($_GET['zp'])) {
   $url = json_decode(decrypt($_GET['zp'], $password_salt))->url;
   $json = file_get_contents('https://node' . $item . '.herokuapp.com/zippy?url=' . $url);
-  echo $json;
+  $decode = json_decode($json);
+  $extension = end(explode($decode, '.'));
+  if($extension === 'mp4') {
+    ini_set("max_execution_time", 0);
+    $stream = new VideoStream($decode->url);
+    $stream->start();
+  } else {
+    echo 'Extension not mp4';
+  }
 } else {
   header("HTTP/1.1 403 Forbidden" );
   exit;
